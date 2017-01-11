c20041d674544b7741af0e9d15d1ee8a80c58f10
jpowleytxd <jorden.powley@zonal.co.uk>
2017-01-11 16:47:16 +0000

    Started TXD new site.

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1.0 user-scalable=no">
+  <meta charset="UTF-8">
+
+  <title>TXD | Digital Marketing Technology</title>
+
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+
+  </header>
+  <section class="front-page">
+      <div class="background homeBack-1" id="homeBack1"></div>
+      <div class="background homeBack-2" id="homeBack2"></div>
+      <div class="hero-statement-container">
+        <h1 class="hero-statement" id="hero-statement">TXD enables brand marketing teams to unlock the power of technology.</h1>
+      </div>
+  </section>
+  <section class="products">
+    <div class="prod-nav-container">
+      <div class="prod-items">
+        <div class="prod-nav-item prod-mobile" id="prod-mobile">
+          <div class="prod-icon" id="prod-mobile-icon">
+          <span class="prod-nav-label">Mobile</span>
+        </div>
+        <div class="prod-nav-item prod-campaigns" id="prod-campaigns">
+          <img src="" class="prod-icon" id="prod-campaigns-icon">
+          <span class="prod-nav-label">Campaigns</span>
+        </div>
+        <div class="prod-nav-item prod-loyalty" id="prod-loyalty">
+          <img src="" class="prod-icon" id="prod-loyalty-icon">
+          <span class="prod-nav-label">Loyalty</span>
+        </div>
+        <div class="prod-nav-item prod-scv" id="prod-scv">
+          <img src="" class="prod-icon" id="prod-scv-icon">
+          <span class="prod-nav-label">SCV</span>
+        </div>
+      </div>
+      <div class="prod-nav-bar">
+        <div class="prod-nav-current"></div>
+      </div>
+    </div>
+    <div class="prod-info-container">
+      <div class="prod-info" id="mobile-info"></div>
+      <div class="prod-info" id="campaigns-info"></div>
+      <div class="prod-info" id="loyalty-info"></div>
+      <div class="prod-info" id="scv-info"></div>
+    </div>
+    <div class="prod-images-container">
+      <div class="prod-image-group mobile-images">
+        <div class="prod-image mobile-1"></div>
+        <div class="prod-image mobile-2"></div>
+        <div class="prod-image mobile-3"></div>
+        <div class="prod-image mobile-4"></div>
+      </div>
+      <div class="prod-image-group campaigns-images">
+        <div class="prod-image campaigns-1"></div>
+        <div class="prod-image campaigns-2"></div>
+        <div class="prod-image campaigns-3"></div>
+        <div class="prod-image campaigns-4"></div>
+      </div>
+      <div class="prod-image-group loyalty-images">
+        <div class="prod-image loyalty-1"></div>
+        <div class="prod-image loyalty-2"></div>
+        <div class="prod-image loyalty-3"></div>
+        <div class="prod-image loyalty-4"></div>
+      </div>
+      <div class="prod-image-group scv-images">
+        <div class="prod-image scv-1"></div>
+        <div class="prod-image scv-2"></div>
+        <div class="prod-image scv-3"></div>
+        <div class="prod-image scv-4"></div>
+      </div>
+    </div>
+  </section>
+  <section class="why">
+  </section>
+  <section class="clients">
+    <div class="client" id="fullers-outer"></div>
+  </section>
+  <footer>
+
+  </footer>
+</body>
+
+</html>
