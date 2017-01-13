3af14313b2ff53b2e5b905e5fd7b336aa1175bfe
jpowleytxd <jorden.powley@zonal.co.uk>
2017-01-13 17:31:48 +0000

    Edits.

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 
 <body>
   <header>
-
   </header>
   <section class="front-page">
       <div class="background homeBack-1" id="homeBack1"></div>
@@ -21,63 +20,6 @@
       <div class="hero-statement-container">
         <h1 class="hero-statement" id="hero-statement">TXD enables brand marketing teams to unlock the power of technology.</h1>
       </div>
-  </section>
-  <section class="products">
-    <div class="prod-nav-container">
-      <div class="prod-items">
-        <div class="prod-nav-item prod-mobile" id="prod-mobile">
-          <div class="prod-icon" id="prod-mobile-icon">
-          <span class="prod-nav-label">Mobile</span>
-        </div>
-        <div class="prod-nav-item prod-campaigns" id="prod-campaigns">
-          <img src="" class="prod-icon" id="prod-campaigns-icon">
-          <span class="prod-nav-label">Campaigns</span>
-        </div>
-        <div class="prod-nav-item prod-loyalty" id="prod-loyalty">
-          <img src="" class="prod-icon" id="prod-loyalty-icon">
-          <span class="prod-nav-label">Loyalty</span>
-        </div>
-        <div class="prod-nav-item prod-scv" id="prod-scv">
-          <img src="" class="prod-icon" id="prod-scv-icon">
-          <span class="prod-nav-label">SCV</span>
-        </div>
-      </div>
-      <div class="prod-nav-bar">
-        <div class="prod-nav-current"></div>
-      </div>
-    </div>
-    <div class="prod-info-container">
-      <div class="prod-info" id="mobile-info"></div>
-      <div class="prod-info" id="campaigns-info"></div>
-      <div class="prod-info" id="loyalty-info"></div>
-      <div class="prod-info" id="scv-info"></div>
-    </div>
-    <div class="prod-images-container">
-      <div class="prod-image-group mobile-images">
-        <div class="prod-image mobile-1"></div>
-        <div class="prod-image mobile-2"></div>
-        <div class="prod-image mobile-3"></div>
-        <div class="prod-image mobile-4"></div>
-      </div>
-      <div class="prod-image-group campaigns-images">
-        <div class="prod-image campaigns-1"></div>
-        <div class="prod-image campaigns-2"></div>
-        <div class="prod-image campaigns-3"></div>
-        <div class="prod-image campaigns-4"></div>
-      </div>
-      <div class="prod-image-group loyalty-images">
-        <div class="prod-image loyalty-1"></div>
-        <div class="prod-image loyalty-2"></div>
-        <div class="prod-image loyalty-3"></div>
-        <div class="prod-image loyalty-4"></div>
-      </div>
-      <div class="prod-image-group scv-images">
-        <div class="prod-image scv-1"></div>
-        <div class="prod-image scv-2"></div>
-        <div class="prod-image scv-3"></div>
-        <div class="prod-image scv-4"></div>
-      </div>
-    </div>
   </section>
   <section class="why">
   </section>
